d01369cd0e8fcd16a607855de811e2d7500dd22f
ORazzak <ORazzak@channel4.co.uk>
2018-03-26 11:55:11 +0100

    TestNG Basics

---
+++ Framework/src/TestNGFiles/Annotatiion2.java
@@ -0,0 +1,30 @@
+package TestNGFiles;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Annotatiion2 {
+
+	@BeforeSuite
+	public void installsoftware()
+	{
+		System.out.println("system enabled");
+	}
+	
+	
+	@Test
+	public void software()
+	{
+		System.out.println("I am the test");
+	}
+	
+	
+	
+	@AfterSuite
+	public void deinstallsoftware()
+	{
+		System.out.println("system disabled");
+	}
+	
+}
---
+++ Framework/src/packTwo/Annotations.java
@@ -0,0 +1,70 @@
+package packTwo;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Annotations {
+	
+	
+	@BeforeMethod// Before each method gets executed the @BeforeMethod will get executed
+	public void UserIdGeneration() 
+	{
+		System.out.println("This block executs before each Test");
+	}
+	
+	
+	@BeforeTest // Before each test gets executed the @Beforetest will get executed 
+	public void DeleteCookies()
+	{
+		System.out.println("This block executes before all test cases");
+		//Delete the cookies and app
+	}
+	
+	
+	@AfterTest
+	public void Cookiesclose()
+	{
+		System.out.println("This block executes after all test cases");
+		//Close the browsers or app
+	}
+	
+	
+	@AfterMethod
+	public void ReportAdding()
+	{
+		System.out.println("This block executs after each Test");
+		
+	}
+	
+	
+	@Test
+	public void FlightBooking()
+	{
+		System.out.println("Executing Test 1");
+
+	}
+	
+	
+	@Test
+	public void FlightCancel()
+	{
+		System.out.println("Executing Test 2");	
+		
+	}
+	
+	
+	/*NOTE! @BeforeMethod is the pre-requisite for every testcase
+	 		@AfterMethod is the post-requisite for every testcase
+	 		5 testcases = 5 times execution
+	 		
+	 		@BeforeTest is the pre-requisite for all the test cases
+	 		@AfterTest is the post-requisite for all the test cases
+	 		5 testcases = 1 single execution 
+	 		
+	*/
+	
+	
+}
